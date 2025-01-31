c881856ef2031a3f69655c1ed6ca9c2cbfc024fd
Ludovic Garon <ludovicgaron54@gmail.com>
2025-01-31 13:08:58 -0500

    added colors to the graph

--- app/core/graph_plate_transmission.py
+++ app/core/graph_plate_transmission.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from matplotlib.animation import FuncAnimation
 from plate_transmission import Plate
 
-plate = Plate()
+plate = Plate(power_in=10)#, h_convection=10000)#, k=20000, h_convection=1000)
 
 # Set up the 3D plot
 # fig, ax = plt.subplots(figsize=(10, 6))
@@ -17,14 +18,14 @@ def update_plot(frame:int, ax: plt.Axes):
     # Refresh the graph
     ax.clear()
     # print(plate.temps[10,10])
-    line = ax.plot_surface(plate.X * 1e3,  plate.Z * 1e3, plate.temps - 273, label="Temperature")
+    line = ax.plot_surface(plate.X * 1e3,  plate.Z * 1e3, plate.temps - 273, label="Temperature", cmap=cm.coolwarm)
     ax.set_xlabel("Position [mm]")
     ax.set_ylabel("Position [mm]")
     ax.set_zlabel("Temperature [°C]")
     ax.set_title("Temperature Distribution Over Time")
     ax.grid()
-    ax.relim()
-    ax.autoscale_view(True, True, True)
+    # ax.relim()
+    # ax.autoscale_view(True, True, True)
     return line,
 
 ani = FuncAnimation(fig, update_plot, fargs=(ax,), interval=10, cache_frame_data=False)
