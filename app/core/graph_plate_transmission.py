2c807c3aea4226361212716bd54a34d2c2a402a4
Ludovic Garon <ludovicgaron54@gmail.com>
2025-01-31 13:50:30 -0500

    started cleanup and trying to accelerate simulation

--- app/core/graph_plate_transmission.py
+++ app/core/graph_plate_transmission.py
@@ -3,10 +3,9 @@ from matplotlib import cm
 from matplotlib.animation import FuncAnimation
 from plate_transmission import Plate
 
-plate = Plate(power_in=10)#, h_convection=10000)#, k=20000, h_convection=1000)
+plate = Plate()
 
 # Set up the 3D plot
-# fig, ax = plt.subplots(figsize=(10, 6))
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
@@ -17,15 +16,11 @@ def update_plot(frame:int, ax: plt.Axes):
     
     # Refresh the graph
     ax.clear()
-    # print(plate.temps[10,10])
-    line = ax.plot_surface(plate.X * 1e3,  plate.Z * 1e3, plate.temps - 273, label="Temperature", cmap=cm.coolwarm)
+    line = ax.plot_surface(plate.X * 1e3,  plate.Z * 1e3, plate.temps - 273, label="Temperature", cmap=cm.plasma)
     ax.set_xlabel("Position [mm]")
     ax.set_ylabel("Position [mm]")
     ax.set_zlabel("Temperature [°C]")
     ax.set_title("Temperature Distribution Over Time")
-    ax.grid()
-    # ax.relim()
-    # ax.autoscale_view(True, True, True)
     return line,
 
 ani = FuncAnimation(fig, update_plot, fargs=(ax,), interval=10, cache_frame_data=False)
--- app/core/plate_transmission.py
+++ app/core/plate_transmission.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Plate:
     def __init__(self, total_time=500, lx=120e-3, lz=120e-3, thickness=1.5e-3, nx=120, nz=120, k=205, rho=2700,
-                 cp=900, h_convection=20, power_in=1.5, ambient_temp=25.0):
+                 cp=900, h_convection=20, power_in=1.5, ambient_temp=25.0, initial_plate_temp=0):
         # Parameters
         self.total_time = total_time  # Total simulation time [s]
         self.lx = lx  # Length [m]
@@ -18,9 +18,9 @@ class Plate:
         self.rho = rho  # Density [kg/m^3]
         self.cp = cp  # Specific heat capacity [J/kg·K]
         self.alpha = k / (rho * cp)  # Thermal diffusivity [m^2/s]
+        print(self.alpha)
 
         self.h_convection = h_convection  # Convection coefficient [W/m^2·K]
-        # h_conv = 0  # Uncomment to remove convection
 
         # Calculated parameters
         self.dx = lx / nx  # Discretization step in x [m]
@@ -47,38 +47,24 @@ class Plate:
         x = np.arange(0, self.nx) * self.dx
         z = np.arange(0, self.nz) * self.dz
         self.X, self.Z = np.meshgrid(x, z)
-        # self.positions = []
-        # for i in range(self.nx):
-        #     for j in range(self.nz):
-        #         self.positions.append((i*self.dx,j*self.dz))
-        # self.positions_z = np.arange(0, self.nz) * self.dz  # Position z vector
 
         # Power input
         self.power_in = power_in  # Power [W]
         self.p_in_location = (int(round((lx / 4) / self.dx)), int(round((lz / 2) / self.dx)))  # Location of power input (quarter of the length)
-        # self.p_in_location_z = int(round((lz / 2) / self.dx))  # Location of power input (halfway for the depth)
         self.powers = np.zeros([nx, nz])
         self.powers[self.p_in_location] = power_in  # Power applied to one element
-        # self.powers_z = np.zeros(nz)
-        # self.powers_z[self.p_in_location_z] = power_in  # Power applied to one element
 
         # Initial conditions
         self.ambient_temp = 273.0 + ambient_temp  # Ambient temperature [K]
-        self.temps = np.full([nx, nz], self.ambient_temp)  # Initial temperature of all elements in x
-        # print(self.temps)
-        # self.temps_z = np.full(nz, self.ambient_temp)  # Initial temperature of all elements in x
+        self.temps = np.full([nx, nz], self.ambient_temp + initial_plate_temp)  # Initial temperature of all elements in x
 
         self.temp_location = (int(round((lx / 2) / self.dx)), int(round((lz / 2) / self.dz)))  # Location of localized heat
         self.thermistance_location = (int(round((3 * lx / 4) / self.dx)), int(round((3 * lz / 4) / self.dz)))  # Location of temperature measurement
-        # self.temp_location_z = int(round((lz / 2) / self.dz))  # Location of localized heat
-        # self.thermistance_location_z = int(round((3 * lz / 4) / self.dz))  # Location of temperature measurement
 
         # Preallocate vectors
-        # self.energy_added = np.zeros(self.nt)
-        # self.energy_loss = np.zeros(self.nt)
-        # self.thermistances = np.zeros(self.nt)
         self.new_temps = np.zeros_like(self.temps)
-        # self.new_temps_z = np.zeros_like(self.temps_z)
+        self.dt_alpha = self.dt / (self.rho * self.cp) * self.k
+        self.dt_conv = self.dt / (self.rho * self.cp) * self.h_convection
         
         
     
@@ -90,114 +76,105 @@ class Plate:
                 if i == 0:  # First column
                     if j == 0: # First row
                         # We have to add right, up and then air to the left and down.
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i, j + 1] - self.temps[i, j]) / self.dz**2 # up
                         
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # down
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_sides / self.volume # left
                     elif j == self.nz - 1: # Last row    
                         # Add right, down and air to the left and up
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i, j - 1] - self.temps[i, j]) / self.dz**2 # down
                         
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_sides / self.volume # left
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # up
                     else:
                         # Add right, up, down and air to the left only
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i, j + 1] - self.temps[i, j]) / self.dx**2 # up
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i, j - 1] - self.temps[i, j]) / self.dx**2 # down
                         
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * (self.area_sides) / self.volume # left
 
                 elif i == self.nx - 1:  # Last element
                     if j == 0: # First row
                         # We have to add left, up and then air to the right and down.
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i - 1, j] - self.temps[i, j]) / self.dx**2 # left
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i, j + 1] - self.temps[i, j]) / self.dx**2 # up
                         
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_sides / self.volume # right
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # down
                     elif j == self.nz - 1: # Last row    
                         # Add left, down and air to the right and up
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i - 1, j] - self.temps[i, j]) / self.dx**2 # left
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i, j - 1] - self.temps[i, j]) / self.dx**2 # down
                         
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_sides / self.volume # right
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # up
                     else:
                         # Add left, up, down and air to the right only
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i - 1, j] - self.temps[i, j]) / self.dx**2 # left
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i, j + 1] - self.temps[i, j]) / self.dx**2 # up
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i, j - 1] - self.temps[i, j]) / self.dx**2 # down
                         
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_sides / self.volume # right
 
                 else:  # Interior elements
                     if j == 0: # First row
                         # We have to add left, right, up and then air down.
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i - 1, j] - self.temps[i, j]) / self.dx**2 # left
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
+                        self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
-                            self.temps[i, j + 1] - self.temps[i, j]) / self.dx**2 # up
+                        self.new_temps[i, j] += self.dt_alpha * (
+                            self.temps[i, j + 1] - self.temps[i, j]) / self.dz**2 # up
                         
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # down
                     elif j == self.nz - 1: # Last row    
                         # Add left right, down and air up
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
-                            self.temps[i - 1, j] - self.temps[i, j]) / self.dx**2 # left
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
-                            self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
-                            self.temps[i, j - 1] - self.temps[i, j]) / self.dx**2 # down
+                        self.new_temps[i, j] += self.dt_alpha * (
+                            (self.temps[i - 1, j] - self.temps[i, j]) + # left
+                            (self.temps[i + 1, j] - self.temps[i, j]) + # right
+                            self.temps[i, j - 1] - self.temps[i, j]) / self.dz**2 # down
                         
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (
+                        self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # up
                     else:
                         # Add left, right, up and down
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
-                            self.temps[i - 1, j] - self.temps[i, j]) / self.dx**2 # left
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
-                            self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
-                            self.temps[i, j + 1] - self.temps[i, j]) / self.dx**2 # up
-                        self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.k * (
-                            self.temps[i, j - 1] - self.temps[i, j]) / self.dx**2 # down
+                        self.new_temps[i, j] += self.dt_alpha * (
+                            (self.temps[i + 1, j] - self.temps[i, j]) + # right
+                            (self.temps[i, j + 1] - self.temps[i, j]) + # up
+                            (self.temps[i, j - 1] - self.temps[i, j]) + # down
+                            (self.temps[i - 1, j] - self.temps[i, j])) / self.dx**2 # left
 
                 # Add power
                 self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.powers[i, j] / self.volume
                 
-                self.new_temps[i, j] += self.dt / (self.rho * self.cp) * self.h_convection * (self.ambient_temp - self.temps[i, j]) * 2 * self.area_top / self.volume
+                self.new_temps[i, j] += self.dt_conv * (self.ambient_temp - self.temps[i, j]) * 2 * self.area_top / self.volume
 
-        self.temps = self.new_temps
-            
-            
-            
-plate = Plate()
+        self.temps[:] = self.new_temps
