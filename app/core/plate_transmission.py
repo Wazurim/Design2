7346375a71be09c66ac820bce1088b98ffecd951
Ludovic Garon <ludovicgaron54@gmail.com>
2025-01-31 16:26:29 -0500

    Replaced x, z, y convention by x, y, z

--- app/core/plate_transmission.py
+++ app/core/plate_transmission.py
@@ -2,16 +2,16 @@ import numpy as np
 
 
 class Plate:
-    def __init__(self, total_time=500, lx=120e-3, lz=120e-3, thickness=1.5e-3, nx=120, nz=120, k=205, rho=2700,
+    def __init__(self, total_time=500, lx=120e-3, ly=120e-3, thickness=1.5e-3, nx=120, ny=120, k=205, rho=2700,
                  cp=900, h_convection=20, power_in=1.5, ambient_temp=25.0, initial_plate_temp=0):
         # Parameters
         self.total_time = total_time  # Total simulation time [s]
         self.lx = lx  # Length [m]
-        self.lz = lz  # Depth  [m]
+        self.ly = ly  # Depth  [m]
         self.thickness = thickness  # Thickness [m]
 
         self.nx = nx  # Number of elements in x
-        self.nz = nz  # Number of elements in z
+        self.ny = ny  # Number of elements in z
 
         # Material properties (for Aluminum)
         self.k = k  # Thermal conductivity [W/m·K]
@@ -23,43 +23,43 @@ class Plate:
         self.h_convection = h_convection  # Convection coefficient [W/m^2·K]
 
         # Calculated parameters
-        self.dx = lx / nx  # Discretization step in x [m]
-        self.dy = thickness  # Thickness in y [m]
-        self.dz = lz / nz  # Discretization step in z [m]
+        self.dx = lx / nx    # Discretization step in x [m]
+        self.dy = ly / ny    # Discretization step in y [m]
+        self.dz = thickness  # Thickness in z [m]
 
         # TODO: Validate if this should change when in 2D (for now we don't even use dt)
         self.dt = self.dx**2 / (8 * self.alpha)  # Time step [s]
         self.nt = round(self.total_time / self.dt)  # Number of time iterations
 
         # Geometry parameters
-        #  z +--------------+       y ^   
-        #   /              /|         |  ↗ z
-        #y +--------------+ +         | /
+        #  y +--------------+       z ^   
+        #   /              /|         |  ↗ y
+        #z +--------------+ +         | /
         #  |              |/          |/
         #  +--------------+ x         +------->x
-        self.area_ends = self.dx * self.dy  # End area [m^2]
-        self.area_sides = self.dy * self.dz  # Side area [m^2]
-        self.area_top = self.dx * self.dz  # Top/bottom area [m^2]
+        self.area_ends = self.dx * self.dz  # End area [m^2]
+        self.area_sides = self.dz * self.dy  # Side area [m^2]
+        self.area_top = self.dx * self.dy  # Top/bottom area [m^2]
         self.volume = self.dx * self.dy * self.dz  # Element volume [m^3]
 
         self.times = np.arange(0, self.nt) * self.dt  # Time vector
         
         x = np.arange(0, self.nx) * self.dx
-        z = np.arange(0, self.nz) * self.dz
-        self.X, self.Z = np.meshgrid(x, z)
+        y = np.arange(0, self.ny) * self.dy
+        self.X, self.Y = np.meshgrid(x, y)
 
         # Power input
         self.power_in = power_in  # Power [W]
-        self.p_in_location = (int(round((lx / 4) / self.dx)), int(round((lz / 2) / self.dx)))  # Location of power input (quarter of the length)
-        self.powers = np.zeros([nx, nz])
+        self.p_in_location = (int(round((lx / 4) / self.dx)), int(round((ly / 2) / self.dx)))  # Location of power input (quarter of the length)
+        self.powers = np.zeros([nx, ny])
         self.powers[self.p_in_location] = power_in  # Power applied to one element
 
         # Initial conditions
         self.ambient_temp = 273.0 + ambient_temp  # Ambient temperature [K]
-        self.temps = np.full([nx, nz], self.ambient_temp + initial_plate_temp)  # Initial temperature of all elements in x
+        self.temps = np.full([nx, ny], self.ambient_temp + initial_plate_temp)  # Initial temperature of all elements in x
 
-        self.temp_location = (int(round((lx / 2) / self.dx)), int(round((lz / 2) / self.dz)))  # Location of localized heat
-        self.thermistance_location = (int(round((3 * lx / 4) / self.dx)), int(round((3 * lz / 4) / self.dz)))  # Location of temperature measurement
+        self.temp_location = (int(round((lx / 2) / self.dx)), int(round((ly / 2) / self.dy)))  # Location of localized heat
+        self.thermistance_location = (int(round((3 * lx / 4) / self.dx)), int(round((3 * ly / 4) / self.dy)))  # Location of temperature measurement
 
         # Preallocate vectors
         self.new_temps = np.zeros_like(self.temps)
@@ -74,10 +74,10 @@ class Plate:
         
         # Compute interior updates using array slicing
         dT_x = (np.roll(self.temps, shift=-1, axis=0) + np.roll(self.temps, shift=1, axis=0) - 2 * self.temps) / self.dx**2
-        dT_z = (np.roll(self.temps, shift=-1, axis=1) + np.roll(self.temps, shift=1, axis=1) - 2 * self.temps) / self.dz**2
+        dT_y = (np.roll(self.temps, shift=-1, axis=1) + np.roll(self.temps, shift=1, axis=1) - 2 * self.temps) / self.dy**2
         
         # Apply only inner propagation
-        self.new_temps[1:-1, 1:-1] += self.dt_alpha * (dT_x[1:-1, 1:-1] + dT_z[1:-1, 1:-1])
+        self.new_temps[1:-1, 1:-1] += self.dt_alpha * (dT_x[1:-1, 1:-1] + dT_y[1:-1, 1:-1])
         
         # Apply convection boundary conditions
         self.new_temps += self.dt_conv * (self.ambient_temp - self.temps) * 2 * self.area_top / self.volume
@@ -94,8 +94,8 @@ class Plate:
         # Ensure edges don't reference out-of-bounds indices
         self.new_temps[0, :] += self.dt_alpha * ((self.temps[1, :] - self.temps[0, :]) / self.dx**2)
         self.new_temps[-1, :] += self.dt_alpha * ((self.temps[-2, :] - self.temps[-1, :]) / self.dx**2)
-        self.new_temps[:, 0] += self.dt_alpha * ((self.temps[:, 1] - self.temps[:, 0]) / self.dz**2)
-        self.new_temps[:, -1] += self.dt_alpha * ((self.temps[:, -2] - self.temps[:, -1]) / self.dz**2)
+        self.new_temps[:, 0] += self.dt_alpha * ((self.temps[:, 1] - self.temps[:, 0]) / self.dy**2)
+        self.new_temps[:, -1] += self.dt_alpha * ((self.temps[:, -2] - self.temps[:, -1]) / self.dy**2)
         self.temps[:] = self.new_temps
         self.current_time += self.dt
         print(self.current_time)
@@ -103,7 +103,7 @@ class Plate:
     
     def update_plate(self):
         for i in range(self.nx):
-            for j in range(self.nz):
+            for j in range(self.ny):
                 self.new_temps[i, j] = self.temps[i, j]
 
                 if i == 0:  # First column
@@ -112,18 +112,18 @@ class Plate:
                         self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
                         self.new_temps[i, j] += self.dt_alpha * (
-                            self.temps[i, j + 1] - self.temps[i, j]) / self.dz**2 # up
+                            self.temps[i, j + 1] - self.temps[i, j]) / self.dy**2 # up
                         
                         self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # down
                         self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_sides / self.volume # left
-                    elif j == self.nz - 1: # Last row    
+                    elif j == self.ny - 1: # Last row    
                         # Add right, down and air to the left and up
                         self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
                         self.new_temps[i, j] += self.dt_alpha * (
-                            self.temps[i, j - 1] - self.temps[i, j]) / self.dz**2 # down
+                            self.temps[i, j - 1] - self.temps[i, j]) / self.dy**2 # down
                         
                         self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_sides / self.volume # left
@@ -153,7 +153,7 @@ class Plate:
                             self.ambient_temp - self.temps[i, j]) * self.area_sides / self.volume # right
                         self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # down
-                    elif j == self.nz - 1: # Last row    
+                    elif j == self.ny - 1: # Last row    
                         # Add left, down and air to the right and up
                         self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i - 1, j] - self.temps[i, j]) / self.dx**2 # left
@@ -184,16 +184,16 @@ class Plate:
                         self.new_temps[i, j] += self.dt_alpha * (
                             self.temps[i + 1, j] - self.temps[i, j]) / self.dx**2 # right
                         self.new_temps[i, j] += self.dt_alpha * (
-                            self.temps[i, j + 1] - self.temps[i, j]) / self.dz**2 # up
+                            self.temps[i, j + 1] - self.temps[i, j]) / self.dy**2 # up
                         
                         self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # down
-                    elif j == self.nz - 1: # Last row    
+                    elif j == self.ny - 1: # Last row    
                         # Add left right, down and air up
                         self.new_temps[i, j] += self.dt_alpha * (
                             (self.temps[i - 1, j] - self.temps[i, j]) + # left
                             (self.temps[i + 1, j] - self.temps[i, j]) + # right
-                            self.temps[i, j - 1] - self.temps[i, j]) / self.dz**2 # down
+                            self.temps[i, j - 1] - self.temps[i, j]) / self.dy**2 # down
                         
                         self.new_temps[i, j] += self.dt_conv * (
                             self.ambient_temp - self.temps[i, j]) * self.area_ends / self.volume # up
