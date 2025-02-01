dfd11cac883fd15c9a5db38b3eca0eb2e19fd37d
Ludovic Garon <ludovicgaron54@gmail.com>
2025-02-01 08:38:46 -0500

    Added text on graph to show current simulation time

--- app/core/graph_plate_transmission.py
+++ app/core/graph_plate_transmission.py
@@ -23,6 +23,7 @@ def update_plot(frame:int, ax: plt.Axes):
     # Refresh the graph
     ax.clear()
     line = ax.plot_surface(plate.X * 1e3,  plate.Y * 1e3, plate.temps - 273, label="Temperature", cmap=cm.plasma)
+    ax.text2D(-0.1, 0.9, f"Simulation Time : {plate.current_time}s", transform=ax.transAxes)
     ax.set_xlabel("Position [mm]")
     ax.set_ylabel("Position [mm]")
     ax.set_zlabel("Temperature [°C]")
@@ -35,9 +36,10 @@ t1.start()
 
 # This updates the graph every interval (ms)
 # The higher the interval, the less frames you will see, but the faster the simulation will be able to update itself
-ani = FuncAnimation(fig, update_plot, fargs=(ax,), interval=100, cache_frame_data=True, save_count=10)
+ani = FuncAnimation(fig, update_plot, fargs=(ax,), interval=300, cache_frame_data=True, save_count=10)
 
 plt.show()
 
+# This makes sure that when we close the graph window, the thread stops.
 stop = True
 t1.join()
