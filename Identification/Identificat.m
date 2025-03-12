8ffbd88716ce28c05e25eaf933b7b64de533bd55
Ludovic Garon <ludovicgaron54@gmail.com>
2025-03-12 17:18:17 -0400

    fixed auto identification

--- Identification/Identificat.m
+++ Identification/Identificat.m
@@ -1,20 +1,24 @@
 % Paramètres
-nom_fichier = "thermistance3_temp_1000sec_0_001Ratio.txt";
+nom_fichier = "essaie2_temp.txt";
 ordre = 2;  % Nombre de pôles et de zéros
 delai_max = 10; % Temps de retard maximal estimé (nombre d'échantillons)
 
 data = importdata(nom_fichier);
-d1 = data(:,1);
-zero_list = zeros(10, 1);
-start_list = ones(10, 1) * 298;
-x = cat(1, zero_list, data(:,1));
-y = cat(1, start_list, data(:,2));
-u = cat(1, zero_list, ones(length(d1),1));
+%d1 = data(:,1);
+%zero_list = zeros(10, 1);
+%start_list = ones(10, 1) * 298;
+%x = cat(1, zero_list, data(:,1));
+%y = cat(1, start_list, data(:,3));
+%u = cat(1, zero_list, ones(length(d1),1));
 
-dat = iddata(y, u);
+u = data(:, 2) - data(1, 2); % Soustrait le point d'operation
+y = data(:, 3) - data(1, 3);
+
+dat = iddata(y, u, 1);
 
 delai_estime = delayest(dat);
 
-[tf, info] = tfest(dat, ordre, ordre-1, 'InputDelay', delai_estime);
+[f_t, info] = tfest(dat, 1, 0, 0);
 
-compare(dat, tf);
+%f_t = tf([-8.9], [104 1]);
+compare(dat, f_t);
